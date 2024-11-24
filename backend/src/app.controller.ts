de7ce769059a6450d27519b37559f4cfe0ce89be
Cherry <87404668+aspiringLilCoder@users.noreply.github.com>
2024-11-24 18:34:36 +0900

    Setup all backend code provided by our lessons

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Address } from 'viem';
+import { MintTokenDto } from './dtos/MintToken.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,45 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('contract-address')
+  getContractAddress() {
+    return { result: this.appService.getContractAddress() };
+  }
+
+  @Get('token-name')
+  async getTokenName() {
+    return { result: await this.appService.getTokenName() };
+  }
+
+  @Get('total-supply')
+  async getTotalSupply() {
+    return { result: await this.appService.getTotalSupply() };
+  }
+
+  @Get('token-balance/:address')
+  async getTokenBalance(@Param('address') address: Address) {
+    return { result: await this.appService.getTokenBalance(address) };
+  }
+
+  @Get('transaction-receipt')
+  async getTransactionReceipt(@Query('hash') hash: string) {
+    return { result: await this.appService.getTransactionReceipt(hash) };
+  }
+
+  @Get('server-wallet-address')
+  getServerWalletAddress() {
+    return { result: this.appService.getServerWalletAddress() };
+  }
+
+  @Get('check-minter-role')
+  async checkMinterRole(@Query('address') address: Address) {
+    return { result: await this.appService.checkMinterRole(address) };
+  }
+
+  @Post('mint-tokens')
+  async mintTokens(@Body() body: MintTokenDto) {
+    return { result: await this.appService.mintTokens(body.address) };
   }
 }
